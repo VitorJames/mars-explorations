0702dc822ba68d47836b1798094fa72d19d8c035
Vitor James <vitorjamescoin@gmail.com>
2023-02-23 08:56:43 -0300

    update

--- src/components/FindImages.vue
+++ src/components/FindImages.vue
@@ -7,7 +7,7 @@
               cols="12"
               lg="8"
             >
-              <v-card class="grey lighten-3 d-flex align-center" light>
+              <v-card class="d-flex align-center" elevation="5" outlined :dark="$vuetify.theme.dark">
                   <v-col cols="12">
                     <v-row>
                       <v-col cols="6" lg="5" sm="5">
@@ -29,6 +29,7 @@
                               v-on="on"
                               hide-details
                               class="pt-0"
+                              color="error"
                             ></v-text-field>
                           </template>
                           <v-date-picker
@@ -64,6 +65,7 @@
                         hide-details
                         prepend-icon="mdi-robot"
                         single-line
+                        color="error"
                       ></v-select>
                       </v-col>
                       <v-col cols="12" md="2" sm="2">
@@ -126,6 +128,7 @@
             <v-dialog
               v-model="dialog"
               max-width="100%"
+              fullscreen
               class="pt-0"
             >
               <v-card>
@@ -146,28 +149,32 @@
                 <v-card-text class="px-0 pb-0">
                   <v-col cols="12" v-if="i > 0">
                     <v-row>
-                      <v-col cols="7">
-                        <img :src="image.img_src" width="100%" height="550" alt="">
+                      <v-col cols="12" lg="7" md="7" sm="7" class="px-2">
+                        <v-img
+                          :src="image.img_src"
+                          width="100%"
+                        ></v-img>
                       </v-col>
-                      <v-col cols="5" class="px-5 pt-5">
-                        <v-card class="pa-2 grey darken-1 mb-4">
-                          <h2 class="white--text">Robô</h2>
+                      <v-col cols="12" lg="5" md="5" sm="5" class="px-2 pt-3">
+                        <v-card class="pa-2 grey darken-1 mb-3">
+                          <h3 class="white--text">Rover</h3>
                         </v-card>
-                        <h1 class="mb-10 font-weight-bolder">{{image.rover.name}}</h1>
-                        <v-card class="pa-2 grey darken-1 mb-4">
-                          <h2 class="white--text">Câmera</h2>
+                        <h2 class="mb-6 font-weight-bolder">{{image.rover.name}}</h2>
+                        <v-card class="pa-2 grey darken-1 mb-3">
+                          <h3 class="white--text">Câmera</h3>
                         </v-card>
-                        <h1 class="mb-10 font-weight-bolder">{{image.camera.full_name}}</h1>
-                        <v-card class="pa-2 grey darken-1 mb-4">
-                          <h2 class="white--text">Sol</h2>
+                        <h2 class="mb-6 font-weight-bolder">{{image.camera.full_name}}</h2>
+                        <v-card class="pa-2 grey darken-1 mb-3">
+                          <h3 class="white--text">Sol</h3>
                         </v-card>
-                        <h1 class="mb-10 font-weight-bolder">{{image.sol}}</h1>
-                        <v-card class="pa-2 grey darken-1 mb-4">
-                          <h2 class="white--text">Data</h2>
+                        <h2 class="mb-6 font-weight-bolder">{{image.sol}}</h2>
+                        <v-card class="pa-2 grey darken-1 mb-3">
+                          <h3 class="white--text">Data</h3>
                         </v-card>
-                        <h1 class="mb-10 font-weight-bolder">{{image.earth_date | moment("DD/MM/YYYY")}}</h1>
-                        <v-btn color="info" target="_blank" :href="image.img_src">
-                          Link da imagem
+                        <h2 class="mb-6 font-weight-bolder">{{image.earth_date | moment("DD/MM/YYYY")}}</h2>
+                        <v-btn color="info" :download="`${image.rover.name}_${image.camera.full_name}_${image.sol}`" block :href="image.img_src" large>
+                          <v-icon>mdi-download</v-icon>
+                          Download da imagem
                         </v-btn>
                       </v-col>
                     </v-row>
@@ -195,6 +202,79 @@
         </template>
       </v-snackbar>
     </div>
+    <v-snackbar
+      v-model="controls"
+      class="mb-10 pa-0"
+      rounded="pill"
+      elevation="20"
+      timeout="-1"
+    >
+      <v-col cols="12" class="pa-0">
+        <v-row>
+          <v-col cols="5" lg="5" sm="5">
+            <v-menu
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              :return-value.sync="date"
+              offset-y
+              class="pa-0"
+              min-width="auto"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="date"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                  hide-details
+                  class="pt-0"
+                  color="error"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="date"
+                no-title
+                scrollable
+                locale="pt-br"
+              >
+                <v-spacer></v-spacer>
+                <v-btn
+                  text
+                  color="primary"
+                  @click="menu = false"
+                >
+                  Cancelar
+                </v-btn>
+                <v-btn
+                  text
+                  color="primary"
+                  @click="$refs.menu.save(date);"
+                >
+                  OK
+                </v-btn>
+              </v-date-picker>
+            </v-menu>
+          </v-col>
+          <v-col cols="5" lg="5" sm="5" class="pt-0">
+            <v-select
+            v-model="rover"
+            :items="rovers"
+            menu-props="auto"
+            label="Rover"
+            hide-details
+            single-line
+            color="error"
+          ></v-select>
+          </v-col>
+          <v-col cols="1" md="2" sm="2">
+            <v-btn color="error" small fab @click="search()">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-snackbar>
     <v-footer dark fixed padless>
       <v-card class="flex" flat tile>
         <v-card-title class="py-0">
@@ -217,6 +297,7 @@
     data: () => ({ 
       drawer: null,
       snackbar: false,
+      controls: false,
       dialog: false,
       rover: null,
       loading:null,
@@ -229,9 +310,17 @@
       images: []
     }),
     created(){
-      
+      this.checkScroll();
     },
     methods:{
+      checkScroll(){
+        var y = window.scrollY;
+        if (y >= 10) {
+          this.controls = true;
+        } else {
+          this.controls = false;
+        }
+      },
       search(){
         if (this.rover == null) {
           this.snackbar = true
