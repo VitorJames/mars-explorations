9788b33e5bf6b4e7a28270829b25d468c44738e1
Vitor James <vitorjames2@gmail.com>
2024-07-03 15:12:03 -0300

    Melhoria na página de rovers

--- src/views/Rovers.vue
+++ src/views/Rovers.vue
@@ -1,58 +1,58 @@
 <template>
   <v-container class="pb-12">
     <v-col cols="12" v-if="!loading">
-      <v-row justify="center" v-for="mission in finishedMissions" :key="mission.name">
+      <v-row justify="center" v-for="mission in rovers" :key="mission">
         <v-col cols="12" xl="6" lg="7" md="8" class="px-1">
           <v-card class="mx-auto" width="100%" outlined>
             <v-list-item three-line>
               <v-list-item-content>
-                <div class="text-h6 mb-4">{{ mission.name }}</div>
+                <div class="text-h6 mb-4">{{ finishedMissions[mission].name }}</div>
 
                 <v-list-item-title class="text-overline mb-1">
                   Lançamento
                 </v-list-item-title>
                 <v-list-item-subtitle class="mb-2">
-                  {{ mission.launch_date | formatDate }}
+                  {{ finishedMissions[mission].launch_date | formatDate }}
                 </v-list-item-subtitle>
 
                 <v-list-item-title class="text-overline mb-1">
                   Chegada à Marte
                 </v-list-item-title>
                 <v-list-item-subtitle class="mb-2">
-                  {{ mission.landing_date | formatDate }}
+                  {{ finishedMissions[mission].landing_date | formatDate }}
                 </v-list-item-subtitle>
 
                 <v-list-item-title class="text-overline mb-1">
                   Status da missão
                 </v-list-item-title>
                 <v-list-item-subtitle class="mb-2">
-                  {{ mission.status | formatStatus }}
+                  {{ finishedMissions[mission].status | formatStatus }}
                 </v-list-item-subtitle>
 
                 <v-list-item-title class="text-overline mb-1">
                   Sol máximo
                 </v-list-item-title>
                 <v-list-item-subtitle class="mb-2">
-                  {{ mission.max_sol }}
+                  {{ finishedMissions[mission].max_sol }}
                 </v-list-item-subtitle>
 
                 <v-list-item-title class="text-overline mb-1">
                   Data da última foto
                 </v-list-item-title>
                 <v-list-item-subtitle class="mb-2">
-                  {{ mission.max_date | formatDate }}
+                  {{ finishedMissions[mission].max_date | formatDate }}
                 </v-list-item-subtitle>
 
                 <v-list-item-title class="text-overline mb-1">
                   Total de fotos
                 </v-list-item-title>
                 <v-list-item-subtitle class="mb-2">
-                  {{ mission.total_photos }}
+                  {{ finishedMissions[mission].total_photos }}
                 </v-list-item-subtitle>
               </v-list-item-content>
 
-              <v-list-item-avatar tile size="120">
-                <v-img :src="require(`../assets/${mission.name}.png`)" contain></v-img>
+              <v-list-item-avatar tile :size="roverPhotoSize">
+                <v-img :src="require(`../assets/${finishedMissions[mission].name}_rover.jpg`)" contain></v-img>
               </v-list-item-avatar>
             </v-list-item>
           </v-card>
@@ -74,26 +74,13 @@ import { format, parseISO } from "date-fns";
 export default {
   data:() => ({
     loading: true,
-    finishedMissions: [
-      {
-        name: "Spirit",
-        landing_date: "2004-01-04",
-        launch_date: "2003-06-10",
-        status: "complete",
-        max_sol: 2208,
-        max_date: "2010-03-21",
-        total_photos: 124550
-      },
-      {
-        name: "Opportunity",
-        landing_date: "2004-01-25",
-        launch_date: "2003-07-07",
-        status: "complete",
-        max_sol: 5111,
-        max_date: "2018-06-11",
-        total_photos: 198439
-      }
-    ]
+    rovers: ['spirit','opportunity','curiosity','perseverance'],
+    finishedMissions: {
+      spirit: {},
+      opportunity: {},
+      curiosity: {},
+      perseverance: {}
+    }
   }),
   filters: {
     formatDate(date) {
@@ -111,17 +98,26 @@ export default {
   created(){
     this.getCuriosityMission();
   },
+  computed: {
+    roverPhotoSize () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return 120
+        case 'sm': return 120
+        case 'md': return 220
+        case 'lg': return 320
+        case 'xl': return 350
+      }
+    },
+  },
   methods:{
-    getCuriosityMission(){
-      this.axios
-          .get(
-            "/v1/manifests/curiosity?api_key=" +
-              this.$apiKey
-          )
+     getCuriosityMission(){
+      this.rovers.forEach((hoverName) => {
+        this.axios
+          .get(`/v1/manifests/${hoverName}?api_key=${this.$apiKey}`)
           .then((response) => {
             var mission = response.data.photo_manifest;
 
-            this.finishedMissions.push({
+            this.finishedMissions[hoverName] = {
               name: mission.name,
               landing_date: mission.landing_date,
               launch_date: mission.launch_date,
@@ -129,12 +125,14 @@ export default {
               max_sol: mission.max_sol,
               max_date: mission.max_date,
               total_photos: mission.total_photos
-            });
+            };
+            
             this.loading = false;
           })
           .catch(() => {
             this.loading = false;
           });
+      });
     }
   }
 };
